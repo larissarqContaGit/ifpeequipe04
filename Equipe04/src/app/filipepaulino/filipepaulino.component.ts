2f9bb4c7d769781445b58929e7111c6e63635987
F1L1P3P4UL1N0 <fpb@discente.ifpe.edu.br>
2019-09-11 09:01:39 -0300

    Criação do componente FilipePaulino

---
+++ Equipe04/src/app/filipepaulino/filipepaulino.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filipepaulino',
+  templateUrl: './filipepaulino.component.html',
+  styleUrls: ['./filipepaulino.component.css']
+})
+export class FilipepaulinoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
